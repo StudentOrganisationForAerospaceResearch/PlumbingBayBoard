91b74f55c1cc46ea0d0814ece4afd8d62c7b8b7f
Christopher Chan <cjchan.chris@gmail.com>
2023-06-03 15:38:06 -0600

    Update SoarProto

--- Components/SoarProtocol/PBBProtocolTask.hpp
+++ Components/SoarProtocol/PBBProtocolTask.hpp
@@ -22,15 +22,20 @@ public:
         return inst;
     }
 
+    static void SendProtobufMessage(EmbeddedProto::WriteBufferFixedSize<DEFAULT_PROTOCOL_WRITE_BUFFER_SIZE>& writeBuffer, Proto::MessageID msgId)
+    {
+		Inst().ProtocolTask::SendProtobufMessage(writeBuffer, msgId);
+    }
+
     void InitTask();
 
 protected:
     static void RunTask(void* pvParams) { PBBProtocolTask::Inst().Run(pvParams); } // Static Task Interface, passes control to the instance Run();
 
     // These handlers will receive a buffer and size corresponding to a decoded message
-    void HandleProtobufCommandMessage(EmbeddedProto::ReadBufferFixedSize<PROTOCOL_RX_BUFFER_SZ_BYTES> readBuffer);
-    void HandleProtobufControlMesssage(EmbeddedProto::ReadBufferFixedSize<PROTOCOL_RX_BUFFER_SZ_BYTES> readBuffer);
-    void HandleProtobufTelemetryMessage(EmbeddedProto::ReadBufferFixedSize<PROTOCOL_RX_BUFFER_SZ_BYTES> readBuffer);
+    void HandleProtobufCommandMessage(EmbeddedProto::ReadBufferFixedSize<PROTOCOL_RX_BUFFER_SZ_BYTES>& readBuffer);
+    void HandleProtobufControlMesssage(EmbeddedProto::ReadBufferFixedSize<PROTOCOL_RX_BUFFER_SZ_BYTES>& readBuffer);
+    void HandleProtobufTelemetryMessage(EmbeddedProto::ReadBufferFixedSize<PROTOCOL_RX_BUFFER_SZ_BYTES>& readBuffer);
 
     // Member variables
 
