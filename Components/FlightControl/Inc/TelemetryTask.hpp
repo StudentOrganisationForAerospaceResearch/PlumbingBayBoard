3bbb1ec995c24602cc9ba23e977234a0fd2d0172
shivamdesai04 <101665374+shivamdesai04@users.noreply.github.com>
2023-09-23 15:40:47 -0600

    comp servo

--- Components/FlightControl/Inc/TelemetryTask.hpp
+++ Components/FlightControl/Inc/TelemetryTask.hpp
@@ -10,7 +10,7 @@
 #include "Task.hpp"
 #include "SystemDefines.hpp"
 
-#define TELEMETRY_DEFAULT_LOGGING_RATE_MS 1000;
+#define TELEMETRY_DEFAULT_LOGGING_RATE_MS 200;
 
 class TelemetryTask : public Task
 {
--- Components/FlightControl/MEV.cpp
+++ Components/FlightControl/MEV.cpp
@@ -14,14 +14,14 @@ bool MEV::servoState = false;
 void MEV::OpenMEV() {
     SOAR_PRINT("closed");
     HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_4);
-    htim8.Instance->CCR4 = 169;  //125   end - 250   open range - 76      187
+    htim8.Instance->CCR4 = 173;  //125   end - 250   open range - 76      187    latest - 175
     servoState = true;
 }
 
 void MEV::CloseMEV() {
     SOAR_PRINT("opened");
     HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_4);
-    htim8.Instance->CCR4 = 78;      //49    end - 45     111
+    htim8.Instance->CCR4 = 104;      //49    end - 45     111    latest - 101
     servoState = false;
 }
 
